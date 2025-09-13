a9670a7b8ba53df74e3232332abba95501a926e3
ibukye <ramnjn0112mk@gmail.com>
2025-09-13 12:26:13 +0800

    adapt to GitHub Actions (CI/CD)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const repositoryName = 'portfolio'; 
+
+const nextConfig = {
+  // 静的サイトとして出力するための設定
+  output: 'export',
+
+  // GitHub Pagesで正しく表示するための設定
+  basePath: `/${repositoryName}`,
+  assetPrefix: `/${repositoryName}/`,
+  
+  // 静的エクスポート時に画像最適化を無効にする設定
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
