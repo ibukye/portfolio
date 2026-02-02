1acef2aa27271d70ddc810cbdac5ebb44a03c391
ibukye <ramnjn0112mk@gmail.com>
2026-02-02 22:56:55 +0800

    updated and added content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,27 +18,46 @@ const AboutPage = () => {
             <li>
               <strong className="block text-lg">The International School@Park City (2023年1月〜2024年8月)</strong>
               <p className="text-gray-600 dark:text-gray-300 mt-1">
-                AS-levelカリキュラム(Year12)にて、Math、Further Maths共に最高成績であるAを取得。<br />
-                A-levelカリキュラム(Year13)にて、Math、Further Maths共に最高成績であるA*を取得。<br />
-                また、Year13時には全国の学生が参加するMath Competitionにおいて、学校内3名のみ獲得したGold Awardを受賞。<br />
+                AS-levelカリキュラム(Year12)にて、Math、Further Maths共に最高評価であるAを取得。<br />
+                A-levelカリキュラム(Year13)にて、Math、Further Maths共に最高評価であるA*を取得。<br />
+                また、Year13時には全国規模のMath Competitionにて、学校内で3名のみが受賞したGold Awardを獲得。<br />
               </p>
             </li>
             {/* Gap Year */}
             <li>
               <strong className="block text-lg">Gap Year (2024年8月〜2025年9月)</strong>
               <p className="text-gray-600 dark:text-gray-300 mt-1">
-                University of Nottinghamに入学するためIELTSの勉強を始める<br />
-                Overall 5.5 (Listening: 6.5, Reading: 6.0, Writing: 5.5, Speaking: 5.0)を獲得<br />
+                University of Nottingham入学に向けてIELTSの学習を開始<br />
+                IELTS Overall 5.5 (Listening: 6.5, Reading: 6.0, Writing: 5.5, Speaking: 5.0)を獲得<br />
               </p>
             </li>
             {/* University of Nottingham */}
             <li>
               <strong className="block text-lg">University of Nottingham (2025年9月〜現在)</strong>
               <p className="text-gray-600 dark:text-gray-300 mt-1">
-                Computer Science with Artificial Intelligence (Faculty of Science) 2年在学中。<br />
-                人工知能やWeb開発に強い関心を持ち、授業に加えて自主制作プロジェクトにも積極的に取り組んでいます。<br />
+                Computer Science with Artificial Intelligence (Faculty of Science) 2年次に在学中。<br />
+                1年時の成績平均は約88％ (Math: 97%) <br />
+                人工知能やWeb開発に強い関心を持ち、授業に加え、自主制作プロジェクトにも積極的に取り組んでいます。<br />
               </p>
             </li>
+            
+            {/* 松尾岩澤研究所 大規模言語モデル講座 2025 基礎編 終了 */}
+            <li>
+              <strong>松尾岩澤研究所 LLM基礎講座 2025 修了</strong>
+              <p>修了書を受け取りました。</p>
+            </li>
+
+            {/* 松尾岩澤研究所 DL基礎講座 2025 Autumn 修了 */}
+            <li>
+              <strong>松尾岩澤研究所 DL基礎講座 2025 Autumn 修了 (2026/1/27)</strong>
+              <p>
+                修了書を受け取りました。 <br/>
+              </p>
+            </li>
+
+
+
+
           </ul>
         </section>
 
@@ -46,7 +65,7 @@ const AboutPage = () => {
           <h2 className="text-2xl font-semibold border-b-2 border-blue-500 pb-2 mb-4">開発経験</h2>
           <ul className="space-y-6">
             <li>
-              <strong className="block text-lg">2025年5月～：手書き数字認識アプリ</strong>
+              <strong className="block text-lg">2025年5月～9月：手書き数字認識アプリ</strong>
               <p className="text-gray-600 dark:text-gray-300 mt-1">
                 <span className="font-semibold">使用技術：</span>Python, MLP, CNN, ResNet, FastAPI  
                 <br />
@@ -61,6 +80,20 @@ const AboutPage = () => {
                 チャット履歴から感情分析を行い、ユーザーに合ったプレゼントを提案するWebアプリをチームで開発しました。
               </p>
             </li>
+
+            <li>
+              <strong className="block text-lg">2025年9月～：Virtual Chemical Reactor</strong>
+              <p className="text-gray-600 dark:text-gray-300 mt-1">
+                <span className="font-semibold">使用技術：</span>Unity, C#, Solidwork 
+                <br />
+                University of Nottingham Malaysiaをクライエントとした、現実で学生全員が使用することが難しい化学反応器を受動的に動画を見て学ぶのではなく主体的に自分でコントロールをしながら学ぶことができるようなシミュレーションゲームを開発しています。
+              </p>
+            </li>
+
+          
+          
+          
+          
           </ul>
         </section>
 
